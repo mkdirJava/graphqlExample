1666106c1192d212177fc442cf442df4e8a81179
mkdirjava <mkdirjava@gmail.com>
2020-05-08 12:33:14 +0100

    first commit

---
+++ src/main/java/com/practice/springboot/graphql/Entry.java
@@ -0,0 +1,12 @@
+package com.practice.springboot.graphql;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Entry {
+	
+	public static void main(String[] args) {
+		SpringBootApplication.run
+	}
+
+}
